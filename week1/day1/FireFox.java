b0c1660503681b29ee526bce08763e553be90158
sangeyuva <147974760+sangeyuva@users.noreply.github.com>
2023-10-15 02:31:47 -0700

    Add files via upload
    Week 1  Day 1

---
+++ week1/day1/FireFox.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class FireFox {
+	public static void main(String[] args) {
+		float browserVersion = 100.2f;
+		String  browserName = "firefox";
+		boolean  isVisible = true;
+		int  releaseYear = 1998;
+		char browserLogo = 'f';
+		System.out.println("Browser Version : " +browserVersion);
+		System.out.println("Browser Name :" +browserName);
+		System.out.println("Visible ? " +isVisible);
+		System.out.println("Release year:" +releaseYear);
+		System.out.println("Browser Logo : " +browserLogo);
+		
+		
+		
+		
+	}
+
+}
